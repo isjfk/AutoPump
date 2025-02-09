3d8a28800055c9fb5bf64517d7688513c47153b0
Jimmy Klarke <iss.jfk@gmail.com>
2025-02-09 15:03:39 +0800

    Initial implementation.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -37,6 +37,15 @@ extern "C" {
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
 
+typedef enum {
+  SensorLevel0,
+  SensorLevel1,
+  SensorLevel2,
+  SensorLevel3,
+  SensorLevel4,
+  SensorLevelInvalid = 255,
+} SensorLevel;
+
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
@@ -57,10 +66,18 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_RED_Pin GPIO_PIN_1
+#define LED_RED_GPIO_Port GPIOA
+#define LED_GREEN_Pin GPIO_PIN_2
+#define LED_GREEN_GPIO_Port GPIOA
+#define LED_BLUE_Pin GPIO_PIN_3
+#define LED_BLUE_GPIO_Port GPIOA
 #define SENSOR0_Pin GPIO_PIN_10
 #define SENSOR0_GPIO_Port GPIOB
 #define SENSOR1_Pin GPIO_PIN_11
 #define SENSOR1_GPIO_Port GPIOB
+#define SENSOR_EMPTY_Pin GPIO_PIN_12
+#define SENSOR_EMPTY_GPIO_Port GPIOB
 #define PUMP_Pin GPIO_PIN_13
 #define PUMP_GPIO_Port GPIOB
 #define VALVE0_Pin GPIO_PIN_14
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,16 +22,40 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+#include <math.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 
+typedef struct {
+  uint32_t startTick;
+  uint32_t targetTick;
+} TimerContext;
+
+typedef struct {
+  uint32_t upPeriodMs;
+  uint32_t downPeriodMs;
+  double threshould;
+  uint32_t prevTick;
+  double accumulateValue;
+  int currValue;
+} FilterContext;
+
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
+#define VALVE_ON_LEVEL            SensorLevel0
+#define VALVE_OFF_LEVEL           SensorLevel4
+#define PUMP_ON_TIMEOUT_MS        (1000 * 60)
+#define TANK_EMPTY_BLINK_MS       (500)
+#define FILTER_UP_PERIOD_MS       (200)
+#define FILTER_DOWN_PERIOD_MS     (1000)
+#define FILTER_THRESHOULD         (0.2)
 
 /* USER CODE END PD */
 
@@ -49,8 +73,19 @@ UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
-uint32_t sensor0Val = 0;
-uint32_t sensor1Val = 0;
+volatile uint16_t sensor0Curr = 0;
+volatile uint16_t sensor0Prev = 0;
+volatile uint16_t sensor0Value = 0;
+FilterContext sensor0Filter;
+volatile SensorLevel sensor0Level = 0;
+
+volatile uint16_t sensor1Curr = 0;
+volatile uint16_t sensor1Prev = 0;
+volatile uint16_t sensor1Value = 0;
+FilterContext sensor1Filter;
+volatile SensorLevel sensor1Level = 0;
+
+FilterContext sensorEmptyFilter;
 
 /* USER CODE END PV */
 
@@ -83,15 +118,115 @@ int fputc(int ch, FILE *f)
   return ch;
 }
 
+void initCycleTime(TimerContext* context, uint32_t cycleTimeMs) {
+  context->startTick = HAL_GetTick();
+  context->targetTick = cycleTimeMs;
+}
+
+bool isOnCycleTime(TimerContext* context) {
+  uint32_t currTick = HAL_GetTick();
+  uint32_t interval = (currTick > context->startTick) ? currTick - context->startTick : 0;
+  if (interval > context->targetTick) {
+    uint32_t tickShift = ((interval / context->targetTick) > 1) ? 0 : (interval % context->targetTick);
+    context->startTick = currTick + tickShift;
+    return true;
+  }
+  return false;
+}
+
+void initAfterTime(TimerContext* context, uint32_t timeMs) {
+  context->startTick = HAL_GetTick();
+  context->targetTick = timeMs;
+}
+
+bool isAfterTime(TimerContext* context) {
+  uint32_t currTick = HAL_GetTick();
+  uint32_t interval = currTick - context->startTick;
+  return interval > context->targetTick;
+}
+
+void initSensorFilter(FilterContext* context, uint32_t upPeriodMs, uint32_t downPeriodMs, double threshould) {
+  context->upPeriodMs = upPeriodMs;
+  context->downPeriodMs = downPeriodMs;
+  context->threshould = threshould;
+  context->prevTick = HAL_GetTick();
+  context->accumulateValue = 0;
+  context->currValue = 0;
+}
+
+int sensorFilter(FilterContext* context, int value) {
+  uint32_t currTick = HAL_GetTick();
+
+  double interval = currTick - context->prevTick;
+  double period = (value >= context->accumulateValue) ? context->upPeriodMs : context->downPeriodMs;
+  double step = (interval < period) ? (interval / period) : 1.0;
+  context->accumulateValue = (context->accumulateValue * (1.0 - step)) + (value * step);
+
+  int currValue = (int) context->accumulateValue;
+  if (fabs(currValue - context->accumulateValue) > context->threshould) {
+    currValue += (currValue >= 0) ? 1 : -1;
+  }
+ 
+  context->prevTick = currTick;
+  context->currValue = currValue;
+
+  return currValue;
+}
+
+#define ledOn(color)    HAL_GPIO_WritePin(LED_##color##_GPIO_Port, LED_##color##_Pin, GPIO_PIN_RESET);
+#define ledOff(color)   HAL_GPIO_WritePin(LED_##color##_GPIO_Port, LED_##color##_Pin, GPIO_PIN_SET);
+#define pumpOn()        HAL_GPIO_WritePin(PUMP_GPIO_Port, PUMP_Pin, GPIO_PIN_SET);
+#define pumpOff()       HAL_GPIO_WritePin(PUMP_GPIO_Port, PUMP_Pin, GPIO_PIN_RESET);
+#define valve0On()      HAL_GPIO_WritePin(VALVE0_GPIO_Port, VALVE0_Pin, GPIO_PIN_SET);
+#define valve0Off()     HAL_GPIO_WritePin(VALVE0_GPIO_Port, VALVE0_Pin, GPIO_PIN_RESET);
+#define valve1On()      HAL_GPIO_WritePin(VALVE1_GPIO_Port, VALVE1_Pin, GPIO_PIN_SET);
+#define valve1Off()     HAL_GPIO_WritePin(VALVE1_GPIO_Port, VALVE1_Pin, GPIO_PIN_RESET);
+
+bool isSensorEmpty() {
+  GPIO_PinState state = HAL_GPIO_ReadPin(SENSOR_EMPTY_GPIO_Port, SENSOR_EMPTY_Pin);
+  return state == GPIO_PIN_SET;
+}
+
+SensorLevel toSensorLevel(uint32_t sensorVal) {
+  if ((sensorVal >= 2800) && (sensorVal < 6000)) {
+    // 4000
+    return SensorLevel0;
+  } else if ((sensorVal >= 1200) && (sensorVal < 2800)) {
+    // 1600
+    return SensorLevel1;
+  } else if ((sensorVal >= 600) && (sensorVal < 1200)) {
+    // 800
+    return SensorLevel2;
+  } else if ((sensorVal >= 300) && (sensorVal < 600)) {
+    // 400
+    return SensorLevel3;
+  } else if ((sensorVal >= 100) && (sensorVal < 300)) {
+    // 200
+    return SensorLevel4;
+  }
+  return SensorLevelInvalid;
+}
+
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
-	//LOG("HAL_TIM_IC_CaptureCallback");
-	if (htim == &htim2) {
-		if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3) {
-			sensor0Val = htim->Instance->CCR3;
-		} else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_4) {
-			sensor1Val = htim->Instance->CCR4;
-		}
-	}
+  if (htim == &htim2) {
+    if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3) {
+      sensor0Curr = htim->Instance->CCR3;
+      if (sensor0Curr != sensor0Prev) {
+        sensor0Value = sensor0Curr - sensor0Prev;
+        SensorLevel level = toSensorLevel(sensor0Value);
+        sensor0Level = sensorFilter(&sensor0Filter, level);
+      }
+      sensor0Prev = sensor0Curr;
+    } else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_4) {
+      sensor1Curr = htim->Instance->CCR4;
+      if (sensor1Curr != sensor1Prev) {
+        sensor1Value = sensor1Curr - sensor1Prev;
+        SensorLevel level = toSensorLevel(sensor1Value);
+        sensor1Level = sensorFilter(&sensor1Filter, level);
+      }
+      sensor1Prev = sensor1Curr;
+    }
+  }
 }
 
 /* USER CODE END 0 */
@@ -104,9 +239,9 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-	uint32_t sensor0Prev = 0;
-	uint32_t sensor1Prev = 0;
-
+  initSensorFilter(&sensor0Filter, FILTER_UP_PERIOD_MS, FILTER_DOWN_PERIOD_MS, FILTER_THRESHOULD);
+  initSensorFilter(&sensor1Filter, FILTER_UP_PERIOD_MS, FILTER_DOWN_PERIOD_MS, FILTER_THRESHOULD);
+  initSensorFilter(&sensorEmptyFilter, FILTER_UP_PERIOD_MS, FILTER_DOWN_PERIOD_MS, FILTER_THRESHOULD);
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -131,28 +266,123 @@ int main(void)
   MX_USART1_UART_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-	//HAL_TIM_Base_Start_IT(&htim2);
-	HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_3);
-	HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_4);
+  ledOff(RED);
+  ledOff(GREEN);
+  ledOff(BLUE);
+
+  //HAL_TIM_Base_Start_IT(&htim2);
+  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_3);
+  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_4);
+
+  LOG("AutoPump initialized!");
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  bool isTankEmpty = false;
+  bool isLedRedOn = false;
+  bool isLedGreenOn = true;
+  bool isValve0On = false;
+  bool isValve1On = false;
+  bool isPumpOn = false;
+  bool isError = false;
+
+  TimerContext pumpOnTimeout;
+  TimerContext tankEmptyCycle;
+
+  TimerContext logCycle;
+  initCycleTime(&logCycle, 500);
   while (1)
   {
+    int sensorEmptyLevel = sensorFilter(&sensorEmptyFilter, isSensorEmpty() ? 1 : 0);
+    bool isPrevTankEmpty = isTankEmpty;
+    bool isPrevPumpOn = isPumpOn;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-		if (sensor0Val != sensor0Prev) {
-			LOG("Sensor0: %u", sensor0Val - sensor0Prev);
-			sensor0Prev = sensor0Val;
-		}
-		if (sensor1Val != sensor1Prev) {
-			LOG("Sensor1: %u", sensor1Val - sensor1Prev);
-			sensor1Prev = sensor1Val;
-		}
-		//HAL_Delay(1000);
-		HAL_IWDG_Refresh(&hiwdg);
+    isTankEmpty = sensorEmptyLevel > 0;
+    
+    // For debug purpose
+    //isTankEmpty = !isTankEmpty;
+
+    if (isError) {
+      isLedRedOn = true;
+      isLedGreenOn = false;
+      isValve0On = false;
+      isValve1On = false;
+      isPumpOn = false;
+    } else if (isTankEmpty) {
+      if (!isPrevTankEmpty) {
+        initCycleTime(&tankEmptyCycle, TANK_EMPTY_BLINK_MS);
+        isLedRedOn = true;
+      } else if (isOnCycleTime(&tankEmptyCycle)) {
+        isLedRedOn = !isLedRedOn;
+      }
+      isLedGreenOn = false;
+      isValve0On = false;
+      isValve1On = false;
+      isPumpOn = false;
+    } else {
+      if (sensor0Level <= VALVE_ON_LEVEL) {
+        isValve0On = true;
+      } else if(sensor0Level >= VALVE_OFF_LEVEL) {
+        isValve0On = false;
+      }
+
+      if (sensor1Level <= VALVE_ON_LEVEL) {
+        isValve1On = true;
+      } else if (sensor1Level >= VALVE_OFF_LEVEL) {
+        isValve1On = false;
+      }
+
+      isPumpOn = isValve0On || isValve1On;
+      if (isPumpOn) {
+        // Check previous pump on state
+        if (!isPrevPumpOn) {
+          // From pump off to pump on, setup timeout
+          initAfterTime(&pumpOnTimeout, PUMP_ON_TIMEOUT_MS);
+        } else if (isAfterTime(&pumpOnTimeout)) {
+          // Pump on timeout, set error flat to keep all valves & pump off
+          isError = true;
+          continue;
+        }
+      }
+
+      isLedRedOn = isPumpOn;
+      isLedGreenOn = true;
+    }
+
+    if (isLedRedOn) {
+      ledOn(RED);
+    } else {
+      ledOff(RED);
+    }
+    if (isLedGreenOn) {
+      ledOn(GREEN);
+    } else {
+      ledOff(GREEN);
+    }
+    if (isValve0On) {
+      valve0On();
+    } else {
+      valve0Off();
+    }
+    if (isValve1On) {
+      valve1On();
+    } else {
+      valve1Off();
+    }
+    if (isPumpOn) {
+      pumpOn();
+    } else {
+      pumpOff();
+    }
+
+    if (isOnCycleTime(&logCycle)) {
+      LOG("S0[%u] S1[%u] SE[%u] | V0[%u] V1[%u] Pump[%u] | R[%u] G[%u] ", sensor0Level, sensor1Level, sensorEmptyLevel, isValve0On, isValve1On, isPumpOn, isLedRedOn, isLedGreenOn);
+    }
+
+    HAL_IWDG_Refresh(&hiwdg);
   }
   /* USER CODE END 3 */
 }
@@ -334,11 +564,27 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_GREEN_Pin|LED_BLUE_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, PUMP_Pin|VALVE0_Pin|VALVE1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_RED_Pin LED_GREEN_Pin LED_BLUE_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_GREEN_Pin|LED_BLUE_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SENSOR_EMPTY_Pin */
+  GPIO_InitStruct.Pin = SENSOR_EMPTY_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(SENSOR_EMPTY_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PUMP_Pin VALVE0_Pin VALVE1_Pin */
   GPIO_InitStruct.Pin = PUMP_Pin|VALVE0_Pin|VALVE1_Pin;
